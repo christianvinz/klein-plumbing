41bde1e7df1c1ab6a5e5a2d284305fde486ab0d9
jessychristianvinz-cloud <jessychristianvinz@icloud.com>
2025-11-12 21:26:05 -0600

    Initialize Storyblok page

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -5,10 +5,44 @@ import {
   apiPlugin
 } from "@storyblok/react/rsc";
 
-// Initialize Storyblok with API plugin
+// Import all components
+import Page from "../../components/Page";
+import Hero from "../../components/Hero";
+import Grid from "../../components/Grid";
+import ServiceCard from "../../components/ServiceCard";
+import BeforeAfterCarousel from "../../components/BeforeAfterCarousel";
+import Testimonial from "../../components/Testimonial";
+import TrustSection from "../../components/TrustSection";
+import JobLog from "../../components/JobLog";
+import ContactSection from "../../components/ContactSection";
+import ServiceDetailPage from "../../components/ServiceDetailPage";
+import FAQAccordion from "../../components/FAQAccordion";
+import DynamicRichTextWrapper from "../../components/DynamicRichTextWrapper";
+
+// Initialize Storyblok with API plugin and components
 storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
   use: [apiPlugin],
+  components: {
+    page: Page,
+    hero: Hero,
+    Hero: Hero,
+    grid: Grid,
+    service_card: ServiceCard,
+    before_after_carousel: BeforeAfterCarousel,
+    testimonial: Testimonial,
+    Testimonial: Testimonial,
+    trust_section: TrustSection,
+    job_log: JobLog,
+    contact_section: ContactSection,
+    service_detail_page: ServiceDetailPage,
+    faq_section: FAQAccordion,
+    rich_text: DynamicRichTextWrapper,
+    slide_pair: () => null,
+    trust_badge: () => null,
+    job_entry: () => null,
+    faq_item: () => null,
+  },
 });
 
 // Tell Next.js this is a dynamic route
